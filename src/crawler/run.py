ba75e5d883dab036b57946b723312930def2eb42
VladCalo <80626362+VladCalo@users.noreply.github.com>
2023-11-30 20:06:34 +0200

    clean up run files

--- src/crawler/run.py
+++ src/crawler/run.py
@@ -3,11 +3,6 @@ from Extraction import Extraction
 from Exploration import Exploration
 
 
-# exp = Exploration(filename='training_data/links.csv')
-# a = exp.get_all_links(initial_links=exp.initial_links, depth=3)
-# print(a)
-# # a = exp.get_recursive_links('https://dunlin.com.au/products/beadlight-cirrus')
-
 process_data = Process_Data(filename="training_data/links.csv")
 process_data.label_training_data(output_file='training_data/training_data.csv')
 
--- src/crawler/run2.py
+++ src/crawler/run2.py
@@ -5,5 +5,3 @@ from Exploration import Exploration
 process_data = Process_Data(filename="test_data/links.csv")
 process_data.preprocess_h_data(file_name="test_data/train_h_data.txt", save_in_file=True)
 
-# a = Exploration(filename="test_data/links.csv")
-# a.get_all_links(initial_links=a.initial_links, depth=3, visited=set())
